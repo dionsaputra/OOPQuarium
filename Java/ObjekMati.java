1828189c8166154b81a1620fac848024987c65b8
dionsaputra <dionsaputra666@gmail.com>
2018-04-12 18:35:07 +0700

    Translate MakananIkan.java and Point.java

---
+++ Java/ObjekMati.java
@@ -0,0 +1,58 @@
+public class ObjekMati{
+    public static int counter = 0;
+    private final int id;
+    private int kecepatan;
+    private Point posisi;
+    private String jenis;
+
+    public ObjekMati(int _kecepatan){
+        id = counter++;
+        kecepatan = _kecepatan;
+        posisi = new Point();
+        jenis = null;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public int getKecepatan(){
+        return kecepatan;
+    }
+
+    public void setKecepatan(int _kecepatan){
+        kecepatan = _kecepatan;
+    }
+
+    public String getJenis(){
+        return jenis;
+    }
+
+    public void setJenis(String _jenis){
+        jenis = _jenis;
+    }
+
+    public Point getPosisi(){
+        return posisi;
+    }
+
+    public void setPosisi(Point _posisi){
+        posisi = _posisi;
+    }
+
+    public boolean isDasar(Aquarium _aquarium){
+        return this.getPosisi().getOrdinat() >= _aquarium.getLength();
+    }
+
+    public void(Aquarium _aquarium){
+        if (!isDasar(_aquarium)) {
+            Point point = this.getPosisi();
+            point.setOrdinat(this.getPosisi().getOrdinat() + this.getKecepatan());
+            this.setPosisi(point);
+        } else {
+            if (this.getJenis() == "Makanan Ikan") {
+                _aquarium.RemoveObject(this);
+            }
+        }
+    }
+}
---
+++ Java/Point.java
@@ -0,0 +1,36 @@
+public class Point {
+    private int absis;
+    private int ordinat;
+
+    public Point(){
+        absis = 0;
+        ordinat = 0;
+    }
+
+    public Point(int _absis, int _ordinat){
+        absis = _absis;
+        ordinat = _ordinat;
+    }
+
+    public int getAbsis(){
+        return absis;
+    }
+
+    public void setAbsis(int _absis){
+        absis = _absis;
+    }
+
+    public int getOrdinat(){
+        return ordinat;
+    }
+
+    public void setOrdinat(int _ordinat){
+        ordinat = _ordinat;
+    }
+
+    public double hitungJarak(Point _point){
+        int dx = this.absis - _point.absis;
+        int dy = this.ordinat - _point.ordinat;
+        return sqrt(dx*dx + dy*dy);
+    }
+}
