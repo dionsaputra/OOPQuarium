8f28113ea689dae4abc777ed297af88ab304871e
Mathias Novianto <mathiasnn98@gmail.com>
2018-04-24 23:59:38 +0700

    added jUnit

---
+++ Inttelijoopquarium/src/GuppyTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GuppyTest {
+
+  @Test
+  void eat() {
+
+  }
+
+  @Test
+  void produce() {
+  }
+
+  @Test
+  void nextGrowth() {
+  }
+
+  @Test
+  void getGrowthStep() {
+  }
+
+  @Test
+  void getFoodCapaty() {
+  }
+
+  @Test
+  void getProduceTime() {
+  }
+
+  @Test
+  void getId() {
+  }
+
+  @Test
+  void getMaxProduceTime() {
+  }
+
+  @Test
+  void setFoodCapacity() {
+  }
+
+  @Test
+  void setProduceTime() {
+  }
+}
