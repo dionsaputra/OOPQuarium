035d9b94c86d4b0d2c9740cf1d538ff9bdf8335e
Mathias Novianto <mathiasnn98@gmail.com>
2018-04-25 20:51:33 +0700

    repaired checkStyle

--- Inttelijoopquarium/src/MouseButtonGuppy.java
+++ Inttelijoopquarium/src/MouseButtonGuppy.java
@@ -1,23 +1,31 @@
+import java.awt.event.MouseEvent;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.MouseListener;
 import java.util.Random;
-
-import javax.swing.*;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class MouseButtonGuppy implements MouseListener,PriceList {
 
   private MachineDriverAquarium aq;
 
+  /**
+   * Constructor.
+   * @param temp machinenya
+   */
   public MouseButtonGuppy(MachineDriverAquarium temp) {
     aq = temp;
   }
 
+  /**
+   * kalo ke klik.
+   * @param e mouseevent
+   */
   public void mouseClicked(MouseEvent e) {
     System.out.println("Mouse Guppy Clicked: (" + e.getX() + ", " + e.getY() + ")");
-    if(aq.getMoney()>=guppyPrice){
+    if (aq.getMoney() >= guppyPrice) {
       Random r = new Random(System.currentTimeMillis());
-      int randomX = r.nextInt(800)+66;
+      int randomX = r.nextInt(800) + 66;
       aq.addGuppy(randomX,50,(JPanel) aq.getContentPane());
       aq.addMoney(-guppyPrice);
     }
